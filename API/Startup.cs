c12d63e611157fa32bcd2ee41a76f3a4e011b820
Joshua Corpuz <joshuacorpuz@Joshuas-Mac-mini.local>
2021-10-27 17:41:36 -0500

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,7 +26,7 @@ namespace API
         {
 
             services.AddControllers();
-            
+            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
